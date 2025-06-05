572858a12eca0036fb7bac6ad8120fde74539bd9
Sanchayan Ghosh <113206213+sanchayan7432@users.noreply.github.com>
2025-06-05 21:36:25 +0530

    Update model_factory.py

--- model_factory.py
+++ model_factory.py
@@ -5,7 +5,7 @@ import os
 class ModelFactory:
     def __init__(self, api_key=None, model_name="gemini-2.0-flash"):
         if api_key is None:
-            api_key = os.getenv("GEMINI_API_KEY") or "AIzaSyDDKvpe02p6R-fYL0lCUDNBLWJRiBR4B78"
+            api_key = os.getenv("GEMINI_API_KEY") or "........"
         if not api_key:
             raise ValueError("API key not provided and GEMINI_API_KEY env variable is not set.")
         genai.configure(api_key=api_key)
